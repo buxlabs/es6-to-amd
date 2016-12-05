e58789d380ebabab1c0fcdcb91ce0c2cbcdd644c
Emil Ajdyna <emil@ajdyna.com>
2016-12-05 19:48:05 +0100

    edit: expose stream api

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
 
 const Module = require('./src/class/Module');
+const stream = require('stream');
 
-module.exports = function (source) {
+function converter (source) {
     var module = new Module(source);
     module.convert();
     return module.toSource();
-};
+}
+
+converter.stream = function () {
+    var transform = new stream.Transform({ objectMode: true });
+
+    transform._transform = function (file, encoding, callback) {
+        callback(null, converter(file));
+    };
+
+    return transform;
+}
+
+module.exports = converter;
