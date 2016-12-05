37a8623d7be7c09a8cdede0f8720fc9ff7caf40c
Emil Ajdyna <emil@ajdyna.com>
2016-12-05 20:41:01 +0100

    add: possibilty to parse vinyl objects

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ converter.stream = function () {
     var transform = new stream.Transform({ objectMode: true });
 
     transform._transform = function (file, encoding, callback) {
-        callback(null, converter(file));
+        if (typeof file === 'string') { return callback(null, converter(file)); }
+        var data = file.contents.toString('utf8');
+        file.contents = new Buffer(converter(data));
+        callback(null, converter(data));
     };
 
     return transform;
