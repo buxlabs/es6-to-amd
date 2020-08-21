f719013d7caf26bb6e7cac6596871480fc89bdd4
Benjamin Franzke <benjaminfranzke@gmail.com>
2020-08-22 00:15:57 +0200

    Add error handling for dynamic imports
    Reject promise for dynamic imports when require fails.
    
    requirejs returns an Error subclass to the error handler.
    therefore we can simply pass the value to the Promise reject()
    method.

--- test/fixture/dynamic-import/output.js
+++ test/fixture/dynamic-import/output.js
@@ -1,19 +1,19 @@
 define(["require", "Foo"], function (require, Bar) {
     "use strict";
-    new Promise(function (resolve) {
-        require(["foo.js"], resolve)
+    new Promise(function (resolve, reject) {
+        require(["foo.js"], resolve, reject)
     }).then(function(foo) {
         console.log(foo);
     });
-    new Promise(function (resolve) {
-        require(["bar" + ".js"], resolve)
+    new Promise(function (resolve, reject) {
+        require(["bar" + ".js"], resolve, reject)
     }).then(function (bar) {
         console.log(bar);
     });
     var dynamic = "foobar";
     async function bar() {
-        var foo = await new Promise(function (resolve) {
-            require([dynamic], resolve)
+        var foo = await new Promise(function (resolve, reject) {
+            require([dynamic], resolve, reject)
         });
         console.log(foo);
     }
